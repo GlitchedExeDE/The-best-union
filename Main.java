d43cb62c486f027c701ee075f03513d99c39c9e6
GlitchedExe <121221621+GlitchedExeDE@users.noreply.github.com>
2023-02-13 18:06:02 +0000

    commit

--- Main.java
+++ Main.java
@@ -1,15 +1,8 @@
+/**
+ * Main
+ */
 public class Main {
-
-    public static int a;
-    public static int b;
-
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-        a = 10;
-        calc();
-    }
-    public static void calc() {
-        b = a - 2;
-        System.out.println("Value of b: "+b);
-    }
+   public static void main(String[] args) {
+      System.out.println("Welcome comrade, to the best union!");
+   }
 }
