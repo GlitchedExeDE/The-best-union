96e1355e44cbfc211dcae022588a57a211272e82
GlitchedExe <121221621+GlitchedExeDE@users.noreply.github.com>
2023-02-14 10:06:31 +0000

    commit

--- Main.java
+++ Main.java
@@ -1,8 +1,20 @@
-/**
- * Main
- */
+import java.util.Scanner;
+
+
 public class Main {
+   public static int a;
    public static void main(String[] args) {
-      System.out.println("Welcome comrade, to the best union!");
+      try (Scanner Input = new Scanner(System.in)) {
+         System.out.println("Welcome comrade, to the best union!");
+         System.out.println("(I)nput 1 for Game");
+         a = Input.nextInt();
+      if(a == 1) {
+         System.out.println("Game selected");
+         game();
+      }
+   }
+   }
+   public static void game() {
+      System.out.println("Welcome to game");
    }
 }
